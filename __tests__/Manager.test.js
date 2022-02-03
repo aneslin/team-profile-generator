042ce015b9e1d4a40f22d32429e01cc3c92afa5b
Aaron Neslin <aneslin125@gmail.com>
2022-02-03 17:12:49 -0500

    added manager class

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -4,9 +4,8 @@ test("check constructor", ()=> {
     manager = new Manager('Bob', 'bob@bob.com', 2,'540')
     expect(manager.name).toEqual(expect.any(String))
     expect(manager.email).toEqual(expect.any(String))
-    expect(manager.role).toBe('Manager')
-    expect(manger.id).toEqual(expect.any(String))
-    expect(manger.office).toEqual(expect.any(String))
+    expect(manager.id).toEqual(expect.any(Number))
+    expect(manager.officeNumber).toEqual(expect.any(String))
 })
 
 test("check manager functions" ,()=>{
